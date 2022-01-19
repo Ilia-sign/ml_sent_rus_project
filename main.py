a83210f2e9371d1cb95cccd00b2596d3d8a11252
ZAYKIN DANIIL <92274248+DancheG14@users.noreply.github.com>
2022-01-19 15:49:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,16 @@ from fastapi import FastAPI
 import torch
 from transformers import AutoModelForSequenceClassification
 from transformers import BertTokenizerFast
-from pydantic import BaseModel
 
-class Item(BaseModel):
-    text: str
+
+
 
 app = FastAPI()
-classifier = pipeline("sentiment-analysis")
 
 @app.get("/")
 def root():
     return {"message": "Hello UrFU"}
 
 @app.post("/predict/")
-def predict(item: Item):
+def predict(item: text):
     return classifier(item.text)[0]
